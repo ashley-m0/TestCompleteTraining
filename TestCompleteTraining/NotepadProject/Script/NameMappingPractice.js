9606f7958df7a628f3cd0de0e4784ac4e6765156
Ashley Mojica <ashleymojica02@gmail.com>
2024-12-16 16:23:08 -0500

    Added NotepadProject

---
+++ TestCompleteTraining/NotepadProject/Script/NameMappingPractice.js
@@ -0,0 +1,13 @@
+﻿function foo()
+{
+
+  // Ugly way
+  //Sys.Process("Notepad").Window("Notepad", "Untitled - Notepad", 1).Window("NotepadTextBox", "", 1).Keys("Hello from Memory.");
+  
+  // Using Name Mapping
+  //NameMapping.Sys.NotepadApp.NotepadMainWindow.NotepadTextBox.Keys("Hello from Name Mapping.");
+  
+  // Using Aliases
+  Aliases.NotepadTextBox.Keys("Hello from Aliases");
+  
+}
